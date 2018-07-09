1ca903c592a5a2664ec3a708d5369e7419ebe791
Shane Honanie <shaneh425@gmail.com>
2018-07-09 14:41:09 -0700

    refactored callback

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
     .options({
@@ -14,22 +15,9 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-    var encodedAddress = encodeURIComponent(argv.address);
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true
-}, (error, response, body) => {
-    if(error) {
-        console.log('unable to connect to google servers');
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('Invalid Address');
-    } else if (body.status === 'OK') {
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Lat: ${body.results[0].geometry.location.lat}`);
-        console.log(`Long: ${body.results[0].geometry.location.lng}`);
-    }
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) 
+        console.log(errorMessage);
     else
-        console.log('else statement');
-    
+        console.log(JSON.stringify(results, undefined, 2));
 });
