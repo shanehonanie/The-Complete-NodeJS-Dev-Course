f44deb5929c311f2b7f5c332d8106dafbf1a2e80
Shane Honanie <shaneh425@gmail.com>
2018-07-10 19:48:19 -0700

    Handlebars Templating

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -2,21 +2,29 @@ const express = require('express');
 const hbs = require('hbs');
 
 var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
-        currentYear: new Date().getFullYear(),
         welcomeMsg: 'Welcome to my new website'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About Page'
     });
 });
 
