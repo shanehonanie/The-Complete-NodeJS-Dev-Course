e3b6e10161786eef89587b377abfb07722b0ac89
Shane Honanie <shaneh425@gmail.com>
2018-07-14 18:58:46 -0700

    Simple Expect Testing

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,15 +1,41 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('Should add two numbers', () => {
     var res = utils.add(33, 11);
 
-    if(res !== 44)
-        throw new Error(`Exprected 44, but got ${res}.`);
+    expect(res).toBe(44).toBeA('number');
+    // if(res !== 44)
+    //     throw new Error(`Exprected 44, but got ${res}.`);
 });
 
 it('Should square a number', () => {
     var res = utils.square(2);
 
-    if(res!== 4)
-        throw new Error(`Expected 4 but got ${res}`)
-})
+    expect(res).toBe(4).toBeA('number');
+    // if(res!== 4)
+    //     throw new Error(`Expected 4 but got ${res}`)
+});
+
+// it('should expect some values', () => {
+//     // expect(12).toNotBe(11);
+//     // expect({name: 'Andrew'}).toNotEqual({name: 'andrew'});
+//     // expect([2,3,4]).toExclude(1);
+//     expect({
+//         name: 'Andrew',
+//         age: 25,
+//         location: 'Philly'
+//     }).toExclude({
+//         age:23
+//     })
+// });
+
+it('Should be set', () => {
+    var user = {location: "Philly", age: 100};
+    var res = utils.setName(user, 'Shane Honanie');
+
+    expect(res).toInclude({
+        firstName: 'Shane', 
+        lastName: 'Honanie'
+    });
+});
