ee01a5b1d6c830ea0f415be730f38e53ff60bae8
Shane Honanie <shaneh425@gmail.com>
2018-07-24 01:22:31 -0700

    ObjectID Timestamp

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,4 +1,8 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if(err)
@@ -16,16 +20,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     console.log(JSON.stringify(result.ops, undefined, 2));
     // });
 
-    db.collection('Users').insertOne({
-        name: 'Shane',
-        age: 100,
-        location: 'Washington'
-    }, (err, result) => {
-        if(err)
-            return console.log('Unable to insert user', err);
+    // db.collection('Users').insertOne({
+    //     name: 'Shane',
+    //     age: 100,
+    //     location: 'Washington'
+    // }, (err, result) => {
+    //     if(err)
+    //         return console.log('Unable to insert user', err);
 
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    });
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // });
 
     db.close();
 });
