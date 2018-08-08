1b77b419d0409656422bc4807341502bebd64a35
Shane Honanie <shaneh425@gmail.com>
2018-08-08 13:42:15 -0700

    Mongoose Validation

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -5,34 +5,46 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-// var newTodo = new Todo({
-//     text: 'Cook dinner'
+// var newTodo2 = new Todo({
+//     text: 'Something to do'
 // });
 
-// newTodo.save().then((doc) => {
-//     console.log('Saved todo', doc);
+// newTodo2.save().then((doc) => {
+//     console.log('Saved todo2', doc);
 // }, (e) => {
-//     console.log('Unable to save todo');
+//     console.log('Unable to save todo2');
 // });
 
-var newTodo2 = new Todo({
-    text: 'Eat dinner',
-    completed: true,
-    completedAt: 123
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 1
+    }
 });
 
-newTodo2.save().then((doc) => {
-    console.log('Saved todo2', doc);
+var newUser = new User({
+    email: 'test@aol.com'
+})
+
+newUser.save().then((doc) => {
+    console.log('Saved newUser', doc);
 }, (e) => {
-    console.log('Unable to save todo2');
+    console.log('Unable to save newUser');
 });
