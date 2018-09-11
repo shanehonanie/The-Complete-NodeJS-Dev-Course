8d3b671373b2de9e4b3b9cf72d6836ee1d958428
Shane Honanie <shaneh425@gmail.com>
2018-09-11 13:52:14 -0700

    Test the DELETE /todos/:id route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -42,6 +42,7 @@ app.get('/todos/:id', (req, res) => {
     Todo.findById(id).then((todo) => {
         if(!todo)
             return res.status(404).send();
+            
         res.send({todo});
     }).catch((e) => {
         res.status(400).send()
@@ -59,7 +60,7 @@ app.delete('/todos/:id', (req, res) => {
         if(!todo)
             return res.status(404).send();
 
-        res.send(todo);
+        res.send({todo});
     }).catch((e) => {
         res.status(400).send();
     });
