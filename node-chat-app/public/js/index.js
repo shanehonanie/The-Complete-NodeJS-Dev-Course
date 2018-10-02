85e402e00ef6497b18f17ea9403cfb64ca7cf6ef
Shane Honanie <shaneh425@gmail.com>
2018-10-02 14:05:20 -0700

    Add newMessafe and createMessage events

---
+++ node-chat-app/public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+        socket.on('connect', function () {
+            console.log('connected to server');
+
+            socket.emit('createMessage', {
+                from: 'Andrew',
+                text: 'yup, that works for me'
+            });
+        });
+
+        socket.on('disconnect', function () {
+            console.log('disconnected from server');
+        });
+
+        socket.on('newMessage', function(message) {
+            console.log('newMessage', message);
+        });
+
--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+        from: 'shane',
+        text: 'hello!',
+        createdAt: 123
+    })
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('user was disconnected');
     });
